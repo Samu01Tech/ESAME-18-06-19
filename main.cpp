38710e991ba8b25c514db99e0b2f7c836035e5f7
Samuele Mazzei <samuele.mazzei@studenti.unitn.it>
2021-02-02 14:55:07 +0000

    stampa battello

--- main.cpp
+++ main.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
 #include <cstdlib>
+#include <cstring>
 using namespace std;
 
+#define DIM 2
+typedef enum Tbattello { CARGO, PASSEGGERI, GUERRA } Tbattello;
+typedef struct Tnave{
+    char nomeNave[20]; unsigned int annoVaro;
+    float stazza;
+    Tbattello tipoBattello;
+    Tnave () {
+        strcpy(&nomeNave, "");
+        annoVaro = 0;
+        stazza = 0.0;
+        tipoBattello = CARGO;
+    }
+    void stampa() const{
+        cout << nomeNave << "(";
+        switch(tipoBattello){
+            case 0: {
+                cout << "CARGO";
+                break;
+            }
+            case 1: {
+                cout << "PASSEGGERI";
+                break;
+            }
+            case 2: {
+                cout << "GUERRA";
+                break;
+            }
+        }
+        cout << ") " << stazza << " TON anno varo=" << annoVaro << endl;
+    }
+} Tnave;
+typedef struct Tnodo {
+    Tnave nave;
+    Tnodo *next, *prev;
+    Tnodo (Tnodo *n, Tnodo *p, Tnave _nave) { /* da implementare */ }
+    Tnodo () { /* da implementare */ }
+    void stampa() { /* da implementare */ }
+} Tnodo; // LISTA LIFO doppiamente concatenata
+
 int main() {
-  std::cout << "Hello World!\n";
+    return 0;
 }
